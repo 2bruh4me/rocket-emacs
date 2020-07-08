41fc48dd873f52238beda25014b275cf52d96ab6
2bruh4me <tg.bruh@protonmail.com>
2020-07-08 14:11:52 +0200

    Remove custom from init.el

--- init.el
+++ init.el
@@ -43,18 +43,3 @@
 (rocket-emacs-init)
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("467c7b8491052b82028f5aef0358663d4e87d866a71e8068e2f3b95032fca207" default))
- '(safe-local-variable-values '((flycheck-disabled-checkers emacs-lisp-checkdoc))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'dired-find-alternate-file 'disabled nil)
